accbface25c418795f9d258ab2d4415e4f120b0b
Your Name <kabutubenson08@gmail.com>
2024-11-11 08:20:19 +0300

    api routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
-import express from "express";
+import express, { application } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -11,7 +13,13 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server listening on port 3000");
 });
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
